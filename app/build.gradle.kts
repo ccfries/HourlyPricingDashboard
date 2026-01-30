e508eb9ad9160b92e240546ebb224e1465de3945
Chris Fries <ccfries@gmail.com>
2026-01-30 11:57:20 -0600

    build: Enable minify and shrink resources

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,9 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            // Enables resource shrinking.
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
